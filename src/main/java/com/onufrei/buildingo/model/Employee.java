63c7d7b820f5c7880a681129d749146dadbae8c2
Artem Onufrei <2hilderoy2@gmail.com>
2021-03-11 09:01:39 +0200

    Made date to display and work properly

--- src/main/java/com/onufrei/buildingo/model/Employee.java
+++ src/main/java/com/onufrei/buildingo/model/Employee.java
@@ -5,6 +5,7 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.Id;
+import org.springframework.format.annotation.DateTimeFormat;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
@@ -29,10 +30,15 @@ public class Employee {
     private String email;
     private String homeAddress;
     private String resumeUrl;
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
     private LocalDate dateOfBirth;
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
     private LocalDate hireDate;
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
     private LocalDate firedDate;
     private EmployeeSpecification specification;
+    @DateTimeFormat(pattern = "yyyy-MM-dd'T'HH:mm")
     private LocalDateTime created_at;
+    @DateTimeFormat(pattern = "yyyy-MM-dd'T'HH:mm")
     private LocalDateTime modified_at;
 }

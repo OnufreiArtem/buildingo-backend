49570ef756cf0c331e56b30b79fbd51bb74b0556
Artem Onufrei <2hilderoy2@gmail.com>
2021-04-01 12:48:59 +0300

    Configured swagger

--- src/main/java/com/onufrei/buildingo/configuration/SwaggerConfiguration.java
+++ src/main/java/com/onufrei/buildingo/configuration/SwaggerConfiguration.java
@@ -1,10 +1,13 @@
 package com.onufrei.buildingo.configuration;
 
-import io.swagger.v3.oas.models.OpenAPI;
-import io.swagger.v3.oas.models.info.Contact;
-import io.swagger.v3.oas.models.info.Info;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import static springfox.documentation.builders.PathSelectors.regex;
 
 /**
  * The representation of the object of SwaggerConfiguration
@@ -13,23 +16,17 @@ import org.springframework.context.annotation.Configuration;
  * @version 1
  * @since 24.03.2021
  */
-
+@EnableSwagger2
 @Configuration
 public class SwaggerConfiguration {
-    public OpenAPI employeeOpenApi() {
-        return new OpenAPI()
-                .info(
-                        new Info()
-                        .title("Building Organization")
-                        .version("1.0")
-                        .contact(
-                                new Contact()
-                            .email("helloworld@mail.com")
-                            .name("My Name")
-                            .url("website.com.ua")
-                        )
 
-                );
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("com.onufrei.buildingo"))
+                .paths(regex("/api.*"))
+                .build();
     }
 
 

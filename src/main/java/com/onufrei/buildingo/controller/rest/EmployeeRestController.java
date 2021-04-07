20be40e34d03fb134e202e341ba6552803048a7e
Artem Onufrei <2hilderoy2@gmail.com>
2021-04-07 10:32:50 +0300

    Added api operations description

--- src/main/java/com/onufrei/buildingo/controller/rest/EmployeeRestController.java
+++ src/main/java/com/onufrei/buildingo/controller/rest/EmployeeRestController.java
@@ -2,6 +2,7 @@ package com.onufrei.buildingo.controller.rest;
 
 import com.onufrei.buildingo.model.Employee;
 import com.onufrei.buildingo.service.employee.interfaces.EmployeeService;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -20,26 +21,31 @@ public class EmployeeRestController {
     @Autowired
     EmployeeService service;
 
+    @ApiOperation(value = "Returns list of employees")
     @GetMapping("/employees")
     List<Employee> getAllEmployees() {
         return service.findAll();
     }
 
+    @ApiOperation(value = "Adds new employee")
     @PostMapping("/employees")
     Employee addEmployee(@RequestBody Employee newEmployee) {
         return service.add(newEmployee);
     }
 
+    @ApiOperation("Gets employee with specified id")
     @GetMapping("/employees/{id}")
     Employee getEmployee(@PathVariable String id) {
         return service.findById(id);
     }
 
+    @ApiOperation("Deletes employee with specified id")
     @DeleteMapping("/employees/{id}")
     Employee deleteEmployee(@PathVariable String id) {
         return service.delete(id);
     }
 
+    @ApiOperation("Updates employee with specified id")
     @PutMapping("/employees/{id}")
     Employee updateEmployee(@RequestBody Employee updatedEmployee, @PathVariable String id) {
         return service.update(id, updatedEmployee);

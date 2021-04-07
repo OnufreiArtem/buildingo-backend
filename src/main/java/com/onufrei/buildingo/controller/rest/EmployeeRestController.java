ebc7e6e63cefcdcb55b0553856058f22aa2ba9a7
Artem Onufrei <2hilderoy2@gmail.com>
2021-04-07 10:57:05 +0300

    Added operations params description

--- src/main/java/com/onufrei/buildingo/controller/rest/EmployeeRestController.java
+++ src/main/java/com/onufrei/buildingo/controller/rest/EmployeeRestController.java
@@ -3,6 +3,7 @@ package com.onufrei.buildingo.controller.rest;
 import com.onufrei.buildingo.model.Employee;
 import com.onufrei.buildingo.service.employee.interfaces.EmployeeService;
 import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -29,25 +30,29 @@ public class EmployeeRestController {
 
     @ApiOperation(value = "Adds new employee")
     @PostMapping("/employees")
-    Employee addEmployee(@RequestBody Employee newEmployee) {
+    Employee addEmployee(@ApiParam(
+                            name = "Employee",
+                            value = "The json of Employee. Id can be null. System will assign it automatically")
+                         @RequestBody Employee newEmployee) {
         return service.add(newEmployee);
     }
 
     @ApiOperation("Gets employee with specified id")
     @GetMapping("/employees/{id}")
-    Employee getEmployee(@PathVariable String id) {
+    Employee getEmployee(@ApiParam(name = "Id", value = "The employee id in UUID format") @PathVariable String id) {
         return service.findById(id);
     }
 
     @ApiOperation("Deletes employee with specified id")
     @DeleteMapping("/employees/{id}")
-    Employee deleteEmployee(@PathVariable String id) {
+    Employee deleteEmployee(@ApiParam(name = "Id", value = "The employee id in UUID format") @PathVariable String id) {
         return service.delete(id);
     }
 
     @ApiOperation("Updates employee with specified id")
     @PutMapping("/employees/{id}")
-    Employee updateEmployee(@RequestBody Employee updatedEmployee, @PathVariable String id) {
+    Employee updateEmployee(@ApiParam(name = "Id", value = "The employee id in UUID format")
+                            @RequestBody Employee updatedEmployee, @PathVariable String id) {
         return service.update(id, updatedEmployee);
     }
 

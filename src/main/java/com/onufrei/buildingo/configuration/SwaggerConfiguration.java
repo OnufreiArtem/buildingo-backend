7fb00c988db34e0ddbe670838321f1301a413407
Artem Onufrei <2hilderoy2@gmail.com>
2021-04-07 10:24:13 +0300

    Added api information

--- src/main/java/com/onufrei/buildingo/configuration/SwaggerConfiguration.java
+++ src/main/java/com/onufrei/buildingo/configuration/SwaggerConfiguration.java
@@ -3,6 +3,8 @@ package com.onufrei.buildingo.configuration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
@@ -26,7 +28,20 @@ public class SwaggerConfiguration {
                 .select()
                 .apis(RequestHandlerSelectors.basePackage("com.onufrei.buildingo"))
                 .paths(regex("/api.*"))
-                .build();
+                .build()
+                .apiInfo(metaInfo());
+    }
+
+    private ApiInfo metaInfo() {
+        return new ApiInfo(
+                "Buildingo Api",
+                "Api for Building Organzation",
+                "1.0",
+                "Terms of Service",
+                new Contact("Artem Onufrei", "https://github.com/OnufreiArtem", "2hilderoy2@gmail.com"),
+                "Apache License Version 2.0",
+                "https://www.apache.org/licenses/LICENSE-2.0"
+        );
     }
 
 

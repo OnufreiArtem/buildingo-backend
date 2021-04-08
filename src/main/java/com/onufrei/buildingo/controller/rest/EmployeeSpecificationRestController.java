bf9a32a7d8c798eb9f8c2486bb07c68bd49b8f37
Artem Onufrei <2hilderoy2@gmail.com>
2021-04-08 09:30:26 +0300

    Implemented operations

--- src/main/java/com/onufrei/buildingo/controller/rest/EmployeeSpecificationRestController.java
+++ src/main/java/com/onufrei/buildingo/controller/rest/EmployeeSpecificationRestController.java
@@ -1,8 +1,11 @@
 package com.onufrei.buildingo.controller.rest;
 
-import com.onufrei.buildingo.repos.EmployeeSpecificationRepository;
+import com.onufrei.buildingo.model.EmployeeSpecification;
+import com.onufrei.buildingo.service.employeeSpecification.interfaces.EmployeeSpecificationService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 /**
  * The representation of the object of EmployeeSpecificationRestController
@@ -11,14 +14,37 @@ import org.springframework.web.bind.annotation.RestController;
  * @version 1
  * @since 08.04.2021
  */
-
+@RequestMapping("/api/v1/employee")
 @RestController
 public class EmployeeSpecificationRestController {
 
     @Autowired
-    EmployeeSpecificationRepository repo;
+    EmployeeSpecificationService service;
 
+    @GetMapping("/specifications/")
+    public List<EmployeeSpecification> getAllEmployeeSpecifications() {
+        return service.findAll();
+    }
 
+    @GetMapping("/specifications/{id}")
+    public EmployeeSpecification getEmployeeSpecificationById(@PathVariable String id) {
+        return service.findById(id);
+    }
 
+    @PostMapping("/specifications/")
+    public EmployeeSpecification addEmployeeSpecification(@RequestBody EmployeeSpecification employeeSpecification) {
+        return service.add(employeeSpecification);
+    }
+
+    @PutMapping("/specifications/{id}")
+    public EmployeeSpecification updateEmployeeSpecification(@RequestBody EmployeeSpecification employeeSpecification,
+                                                          @PathVariable String id) {
+        return service.update(id, employeeSpecification);
+    }
+
+    @DeleteMapping("/specifications/{id}")
+    public EmployeeSpecification deleteEmployeeSpecification(@PathVariable String id) {
+        return service.delete(id);
+    }
 
 }

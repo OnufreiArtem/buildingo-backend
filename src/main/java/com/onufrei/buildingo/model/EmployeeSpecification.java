40549bee62bc3637d16e658067589c3f4ca996d2
Artem Onufrei <2hilderoy2@gmail.com>
2021-04-08 09:52:26 +0300

    Added swagger description for fields

--- src/main/java/com/onufrei/buildingo/model/EmployeeSpecification.java
+++ src/main/java/com/onufrei/buildingo/model/EmployeeSpecification.java
@@ -1,5 +1,6 @@
 package com.onufrei.buildingo.model;
 
+import io.swagger.annotations.ApiModelProperty;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -14,17 +15,27 @@ import java.time.LocalDateTime;
  * @since 03.03.2021
  */
 
+/*
+    TODO: Add time format for create_at and modified_at
+ */
+
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
 public class EmployeeSpecification {
+    @ApiModelProperty(notes="Employee specification Id in UUID format")
     @Id
     private String id;
+    @ApiModelProperty(notes="Employee specification name")
     private String name;
+    @ApiModelProperty(notes="Employee specification description")
     private String description;
+    @ApiModelProperty(notes="Employee specification salary")
     private int salary;
+    @ApiModelProperty(notes="The date when Employee specification was created in yyyy-MM-dd format")
     private LocalDateTime created_at;
+    @ApiModelProperty(notes="The date when Employee specification was lastly modified in yyyy-MM-dd format")
     private LocalDateTime modified_at;
 }

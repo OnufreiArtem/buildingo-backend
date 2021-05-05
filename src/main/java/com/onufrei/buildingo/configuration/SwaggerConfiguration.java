137b76150e07cc62163878909bab07afdf112be4
Artem Onufrei <2hilderoy2@gmail.com>
2021-05-05 23:16:43 +0300

    Solved some dependencies problems

--- src/main/java/com/onufrei/buildingo/configuration/SwaggerConfiguration.java
+++ src/main/java/com/onufrei/buildingo/configuration/SwaggerConfiguration.java
@@ -2,14 +2,13 @@ package com.onufrei.buildingo.configuration;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
 import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
-
-import static springfox.documentation.builders.PathSelectors.regex;
 
 /**
  * The representation of the object of SwaggerConfiguration
@@ -27,7 +26,7 @@ public class SwaggerConfiguration {
         return new Docket(DocumentationType.SWAGGER_2)
                 .select()
                 .apis(RequestHandlerSelectors.basePackage("com.onufrei.buildingo"))
-                .paths(regex("/api.*"))
+                .paths(PathSelectors.regex("/api.*"))
                 .build()
                 .apiInfo(metaInfo());
     }
